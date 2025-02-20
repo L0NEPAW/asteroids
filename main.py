2ab356d984a9296acb3086892c4519a13ba32539
L0NEPAW <jps_gaming@proton.me>
2025-02-19 21:01:03 -0500

    Asteroid split

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.check_collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         for asteroid in asteroids:
